d1039586999072f6e5f7c2e9732ab2fa978e77bf
AlejandroGasca09 <alejandrogascacorona@gmail.com>
2024-08-26 11:22:45 -0600

    Pruebas de prompt

---
+++ promt.js
@@ -0,0 +1,6 @@
+let nombre = prompt("Ingresa tu nombre : ");
+   console.log(nombre);
+let nacionalidad = prompt("Ingresa tu nacionalidad : ");
+   console.log(nacionalidad);
+let edad = prompt("Ingresa tu edad : ");
+   console.log(edad);
