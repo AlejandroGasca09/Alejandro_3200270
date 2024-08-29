c28b1929e3d8426fc224e664be46bb39a9d5aa70
AlejandroGasca09 <119777270+AlejandroGasca09@users.noreply.github.com>
2024-08-28 18:39:09 -0600

    Add files via upload

--- suma.js
+++ suma.js
@@ -1,6 +1,6 @@
 function suma(){
     let numero1 = Number(prompt("Ingresa el numero 1 :"));
-    let numero2 = Number(prompt("Ingresa el numero 1 :"));
+    let numero2 = Number(prompt("Ingresa el numero 2 :"));
     let resultado = numero1 + numero2
     console.log(resultado)
 }
